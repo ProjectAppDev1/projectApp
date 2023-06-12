27903c826f9021f3715c85fb7ddc3ecf9583c985
Meshi Mizrahi <133687753+MeshiMizrahi@users.noreply.github.com>
2023-06-12 16:39:36 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-app.set('view engine', 'ejs');
-//Routes
-app.use('/', require('./routes/login'));
-const PORT = process.env.PORT || 4111;
-app.listen(PORT, console.log("Server has started at port " + PORT))
